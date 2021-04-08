54eea57417e5a4e7d9cbb5eb52a83b88c23dae21
L-oris <loris4526@gmail.com>
2021-04-08 21:09:43 +0200

    get headers from pkg-config stdout

--- main.c
+++ main.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <json-c/json.h>
 #include "configurations.h"
 
 #define PATH_TO_JSON_FILE "../.vscode/c_cpp_properties.json"
 
-int main(void)
+// example program: `libmongoc-1.0`
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "one argument required\n");
+        return 1;
+    }
+    char command_buffer[100];
+    sprintf(command_buffer, "pkg-config --cflags %s", argv[1]);
+    printf("DEBUG -- %s\n", command_buffer);
+
+    FILE *pkg_config_file = popen(command_buffer, "r");
+    if (!pkg_config_file)
+    {
+        fprintf(stderr, "Failed to execute command `%s`\n", command_buffer);
+        return 1;
+    }
+
+    // TODO LORIS: allow writing as much as you can in dynamically allocated string?
+    char pkg_config_stdout[2048];
+    fgets(pkg_config_stdout, sizeof(pkg_config_stdout), pkg_config_file);
+    printf("Successful:\n%s\n", pkg_config_stdout);
+
+    pclose(pkg_config_file);
+    return 0;
+}
+
+int old_main(void)
 {
     json_object *root = json_object_from_file(PATH_TO_JSON_FILE);
     if (!root)
